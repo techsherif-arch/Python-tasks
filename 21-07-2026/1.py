61eee57d6dab12a79b9b5573d5c7e7236e01c838
M Mohamed Sherif <sherif22118@gmail.com>
2026-07-21 20:26:39 +0530

    Add files via upload

---
+++ 21-07-2026/1.py
@@ -0,0 +1,6 @@
+appointment = input("Do you have an appointment?(yes/no)")
+if appointment == "yes":
+    print("You can leave your bike for service today.")
+else:
+    print("You cannot leave your bike for service today.")
+    print("Please book an appointment and visit again.")
